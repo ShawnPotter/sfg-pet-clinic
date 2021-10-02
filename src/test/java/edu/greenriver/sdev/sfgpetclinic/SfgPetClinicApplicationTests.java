103d10d782336cd9c3ec5112bc4ef799e4542308
Shawn Potter <potter.shawn@comcast.net>
2021-10-01 17:03:04 -0700

    Initial Project Add

---
+++ src/test/java/edu/greenriver/sdev/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,15 @@
+package edu.greenriver.sdev.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
